23bf801f415457f2a2882c6d6b56146d93381a37
Kovercrosser <m.weber-1999@hotmail.de>
2024-03-04 17:11:06 +0100

    Refactor pack method in FiletypeServiceZip

--- services/filetype/filetypeServiceZip.py
+++ services/filetype/filetypeServiceZip.py
@@ -3,32 +3,20 @@ from datetime import datetime
 from stream_zip import stream_zip, ZIP_64
 from services.filetype.filetypeBase import FiletypeBase
 
-
-def getFileChunks(filepath):
-    with open(filepath, "rb") as f:
-        while True:
-            chunk = f.read(1024)  # Adjust chunk size as needed
-            if not chunk:
-                break
-            yield chunk
-
-def createZip(filenames):
-    memberFiles = []
-    for filename in filenames:
-        modifiedAt = datetime.now()
-        mode = S_IFREG | 0o600
-        memberFiles.append(
-            (filename, modifiedAt, mode, ZIP_64, getFileChunks(filename))
-        )
-    zippedChunks = stream_zip(memberFiles)
-    return zippedChunks
-
 class FiletypeServiceZip(FiletypeBase):
     def pack(self, files: list[str], chunkSize:int):
         if chunkSize < 512:
             raise ValueError("chunkSize must be at least 512")
-        return createZip(files)
-    
+        memberFiles = []
+        for file in files:
+            modifiedAt = datetime.now()
+            mode = S_IFREG | 0o600
+            memberFiles.append(
+                (file, modifiedAt, mode, ZIP_64, open(file, "rb"))
+            )
+        zippedChunks = stream_zip(files=memberFiles, chunk_size=chunkSize)
+        return zippedChunks
+
 
     def unpack(self, data):
         return data
