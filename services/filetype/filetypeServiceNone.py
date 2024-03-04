553126ce0642fa883e20fa11152f5a4fec933c3f
Kovercrosser <m.weber-1999@hotmail.de>
2024-03-04 17:11:35 +0100

    Linter fixes

--- services/filetype/filetypeServiceNone.py
+++ services/filetype/filetypeServiceNone.py
@@ -1,8 +1,8 @@
 from services.filetype.filetypeBase import FiletypeBase
 
 class FiletypeServiceNone(FiletypeBase):
-    def pack(self, data):
-        return data
+    def pack(self, files: list[str], chunkSize:int):
+        return
 
     def unpack(self, data):
         return data
--- services/transfer/transferServiceDryrun.py
+++ services/transfer/transferServiceDryrun.py
@@ -3,11 +3,11 @@ from services.transfer.transferBase import TransferBase
 class TransferServiceDryrun(TransferBase):
     def upload(self, data):
         size = 0
-        with open('output.zip', 'wb') as f:
+        with open('output.zip', 'wb') as file:
             for chunk in data:
                 size += len(chunk)
                 print(f"Writing {len(chunk)} bytes to output.tar.gzip.aes")
-                f.write(chunk)
+                file.write(chunk)
         print(f"Upload complete. {size} bytes written to output.zip")
 
     def download(self, data):
