6b46cf5e5755d32c4929be3ab80de85eec5b2aed
Kovercrosser <m.weber-1999@hotmail.de>
2024-03-27 17:37:38 +0100

    Update service names in upload_executer.py

--- upload_executer.py
+++ upload_executer.py
@@ -9,10 +9,10 @@ from utils.storage_utils import get_all_files_from_directories_and_files, read_s
 def upload(service: Service, profile: str, paths: list) -> int:
     vault = read_settings(profile, "vault")
 
-    transfer_service: TransferBase = service.get_service("transferService")
-    compression_service: CompressionBase = service.get_service("compressionService")
-    encryption_service: EncryptionBase = service.get_service("encryptionService")
-    filetype_service: FiletypeBase = service.get_service("filetypeService")
+    transfer_service: TransferBase = service.get_service("transfer_service")
+    compression_service: CompressionBase = service.get_service("compression_service")
+    encryption_service: EncryptionBase = service.get_service("encryption_service")
+    filetype_service: FiletypeBase = service.get_service("filetype_service")
 
     files = get_all_files_from_directories_and_files(paths)
     print(f"Uploading {paths} to {vault}...")
