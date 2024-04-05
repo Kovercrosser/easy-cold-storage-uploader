cf6009d2b2c9cb4b8b2eaa1e38797ba6b024d6be
Kovercrosser <m.weber-1999@hotmail.de>
2024-04-05 21:09:40 +0200

    Refactor AWS credentials handling in aws_utils.py

--- utils/aws_utils.py
+++ utils/aws_utils.py
@@ -1,34 +1,26 @@
 import os
 
 def store_aws_credentials(access_key, secret_key, profile_name='default'):
-    # Determine the home directory
     home = os.path.expanduser("~")
-
-    # Construct the .aws directory path
     aws_dir = os.path.join(home, '.aws')
     if not os.path.exists(aws_dir):
         os.makedirs(aws_dir)
-
-    # Path to the credentials file
     credentials_file = os.path.join(aws_dir, 'credentials')
 
     # Write the credentials
-    with open(credentials_file, 'w', encoding='utf-8') as file:
+    with open(credentials_file, 'a', encoding='utf-8') as file:
         file.write(f'[{profile_name}]\n')
         file.write(f'aws_access_key_id = {access_key}\n')
         file.write(f'aws_secret_access_key = {secret_key}\n')
 
 def check_aws_credentials(profile_name='default'):
-    # Determine the home directory
     home = os.path.expanduser("~")
-
-    # Construct the .aws directory path
     aws_dir = os.path.join(home, '.aws')
     if not os.path.exists(aws_dir):
         os.makedirs(aws_dir)
-
-    # Path to the credentials file
     credentials_file = os.path.join(aws_dir, 'credentials')
+    if not os.path.exists(credentials_file):
+        return False
 
     # Write the credentials
     with open(credentials_file, 'r', encoding='utf-8') as file:
