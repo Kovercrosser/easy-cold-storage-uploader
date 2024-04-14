9e72079cf2fa5bb02948295820f759baf3b78985
Restyled.io <commits@restyled.io>
2024-04-14 20:28:45 +0000

    Restyled by autopep8

--- upload_executer.py
+++ upload_executer.py
@@ -11,8 +11,10 @@ def upload(service: Service, profile: str, paths: list) -> int:
     vault = read_settings(profile, "vault")
 
     transfer_service: TransferBase = service.get_service("transfer_service")
-    compression_service: CompressionBase = service.get_service("compression_service")
-    encryption_service: EncryptionBase = service.get_service("encryption_service")
+    compression_service: CompressionBase = service.get_service(
+        "compression_service")
+    encryption_service: EncryptionBase = service.get_service(
+        "encryption_service")
     filetype_service: FiletypeBase = service.get_service("filetype_service")
     print("why")
     with rich_console.status("[bold green]Gathering information about the files..."):
@@ -22,7 +24,7 @@ def upload(service: Service, profile: str, paths: list) -> int:
         return 1
     files_text = "files" if len(files) > 1 else "file"
     rich_console.print(f"Trying to upload {len(files)} {files_text} to [bold purple]{vault}[/bold purple]"
-                        f" using profile: [bold purple]{profile}[/bold purple]")
+                       f" using profile: [bold purple]{profile}[/bold purple]")
 
     packed_generator = filetype_service.pack(files)
     compressed_generator = compression_service.compress(packed_generator)
