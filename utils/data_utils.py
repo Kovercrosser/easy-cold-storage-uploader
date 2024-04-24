c4e502073a03a8a7d8352d36d6972df831031ac2
weber-man <github@weberman.de>
2024-04-24 22:05:17 +0200

    savegame

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -4,13 +4,14 @@ from typing import Generator
 
 class CreateSplittedFilesFromGenerator:
     """
-    This Class  is used to create parts of a file from a generator.
+    This Class is used to create parts of a file from a generator.
     It is similar to a Generator but gives out the chunks in a file of a given size, instead of yielding the chunks.
     """
     remaining_bytes_from_last_part: bytes = bytes()
     generator_exhausted: bool = False
     total_read_bytes: int = 0
     total_written_bytes: int = 0
+    has_returned: bool = False
 
     def create_next_upload_size_part(self, data: Generator[bytes,None,None], upload_size_bytes: int, temp_file: BufferedRandom) -> None:
         """
@@ -23,19 +24,36 @@ class CreateSplittedFilesFromGenerator:
         current_written_bytes = 0
 
         if not temp_file:
-            raise ValueError("tempFile is not set")
+            raise ValueError("temp_ile is not set")
         if upload_size_bytes <= 0:
-            raise ValueError("uploadSizeBytes must be greater than 0")
+            raise ValueError("upload_size_bytes must be greater than 0")
 
         chunk: bytes = bytes()
+        itteration = 0
         while True:
-            try:
-                chunk = next(data)
-                self.total_read_bytes += len(chunk)
-            except StopIteration as exception:
-                self.generator_exhausted = True
-                if self.remaining_bytes_from_last_part == b"" and len(chunk) == 0:
-                    raise exception
+            itteration += 1
+            if len(self.remaining_bytes_from_last_part) < upload_size_bytes:
+                try:
+                    chunk = next(data)
+                    self.total_read_bytes += len(chunk)
+                    if len(chunk) == 0:
+                        raise StopIteration
+                except StopIteration as exception:
+                    self.generator_exhausted = True
+                    if self.remaining_bytes_from_last_part == b"" and len(chunk) == 0:
+                        if not self.has_returned:
+                            temp_file.seek(0)
+                            self.has_returned = True
+                            return
+                        raise exception
+            else:
+                temp_file.write(self.remaining_bytes_from_last_part[:upload_size_bytes])
+                current_written_bytes += upload_size_bytes
+                self.total_written_bytes += upload_size_bytes
+                self.remaining_bytes_from_last_part = self.remaining_bytes_from_last_part[upload_size_bytes:]
+                temp_file.seek(0)
+                self.has_returned = True
+                return
 
             if len(self.remaining_bytes_from_last_part) > upload_size_bytes:
                 temp_file.write(self.remaining_bytes_from_last_part[:upload_size_bytes])
@@ -43,6 +61,7 @@ class CreateSplittedFilesFromGenerator:
                 self.total_written_bytes += upload_size_bytes
                 self.remaining_bytes_from_last_part = self.remaining_bytes_from_last_part[upload_size_bytes:]
                 temp_file.seek(0)
+                self.has_returned = True
                 return
 
             # now self.remainingBytesFromLastPart is empty or smaller then uploadSizeBytes
@@ -57,14 +76,16 @@ class CreateSplittedFilesFromGenerator:
                 if len(chunk) > max_write_size_allowed:
                     temp_file.write(chunk[:max_write_size_allowed])
                     self.remaining_bytes_from_last_part = chunk[max_write_size_allowed:]
+                    chunk = bytes()
                     current_written_bytes += max_write_size_allowed
                     self.total_written_bytes += max_write_size_allowed
                     temp_file.seek(0)
+                    self.has_returned = True
                     return
                 temp_file.write(chunk)
                 current_written_bytes += len(chunk)
                 self.total_written_bytes += len(chunk)
+                chunk = bytes()
 
-            if self.remaining_bytes_from_last_part == b"" and self.generator_exhausted:
-                temp_file.seek(0)
-                return
+            if self.remaining_bytes_from_last_part == b"" and self.generator_exhausted and len(chunk) == 0:
+                raise StopIteration
