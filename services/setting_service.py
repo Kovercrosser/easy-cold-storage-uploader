b0a0a01ca62caf81cdfdc209aa75f64c4ce50ed6
weber-man <github@weberman.de>
2024-05-05 15:27:20 +0200

    saving settings via Tinydb

--- services/setting_service.py
+++ services/setting_service.py
@@ -1,53 +1,24 @@
-import json
-import os
-
+from tinydb import TinyDB, where
 from dependency_injection.service import Service
 from services.db_service import DbService
 
 class SettingService:
-    db_service: DbService
+    db: TinyDB
     def __init__(self, service: Service) -> None:
-        self.db_service = service.get_service("settings_db_service")
+        db_service: DbService = service.get_service("settings_db_service")
+        assert db_service is not None
+        self.db = db_service.get_context()
 
     def store_settings(self, profile: str, key: str, value: str) -> None:
-        home = os.path.expanduser("~")
-        ecsu_dir = os.path.join(home, '.ecsu')
-        if not os.path.exists(ecsu_dir):
-            os.makedirs(ecsu_dir)
-
-        # Path to the settings file
-        settings_file = os.path.join(ecsu_dir, 'settings.json')
-        if not os.path.exists(settings_file):
-            with open(settings_file, 'w', encoding='utf-8') as file:
-                file.write('{}')
-
-        settings_data = ""
-        with open(settings_file, 'r', encoding='utf-8') as file:
-            settings_data = file.read()
-
-        settings_json = json.loads(settings_data) if settings_data else {}
-        if profile not in settings_json:
-            settings_json[profile] = {}
-        settings_json[profile][key] = value
-        settings_data = json.dumps(settings_json)
-
-        with open(settings_file, 'w', encoding='utf-8') as file:
-            file.write(settings_data)
+        profiles_table = self.db.table("profiles")
+        profiles_table.upsert({"profile": profile, "key": key, "value": value}, (where('profile') == profile) & (where('key') == key))
 
     def read_settings(self, profile: str, key: str) -> str | None:
-        home = os.path.expanduser("~")
-        ecsu_dir = os.path.join(home, '.ecsu')
-        if not os.path.exists(ecsu_dir):
-            os.makedirs(ecsu_dir)
-
-        # Path to the settings file
-        settings_file = os.path.join(ecsu_dir, 'settings.json')
-        if not os.path.isfile(settings_file):
-            return None
-        try:
-            with open(settings_file, 'r', encoding='utf-8') as file:
-                settings_data = file.read()
-                settings_json = json.loads(settings_data)
-                return str(settings_json[profile][key])
-        except Exception:
-            return None
+        profiles_table = self.db.table("profiles")
+        result = profiles_table.get((where('profile') == profile) & (where('key') == key))
+        if isinstance(result, dict):
+            return result.get('value')
+        elif isinstance(result, list):
+            if isinstance(result[0], dict):
+                return result[0].get('value')
+        return None
