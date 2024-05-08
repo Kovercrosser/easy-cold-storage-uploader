6636ae0b4483de95ed1893d8302b0e6481387232
weber-man <github@weberman.de>
2024-05-08 20:28:55 +0200

    refactor of cmd parser

--- cmd_parser.py
+++ cmd_parser.py
@@ -12,7 +12,7 @@ def upload_argument_parser(parser_upload: argparse.ArgumentParser) -> argparse.A
         choices=["none", "lzma"],
         default="none",
         help='The compression-method to use.',
-        )
+    )
     parser_upload.add_argument(
         '-l',
         '--compression-level',
@@ -20,7 +20,7 @@ def upload_argument_parser(parser_upload: argparse.ArgumentParser) -> argparse.A
         choices=range(1, 10),
         default=7,
         help='The compression level to use. 1 is lowest, 9 is highest',
-        )
+    )
     # Encryption
     parser_upload.add_argument(
         '-e',
@@ -28,16 +28,17 @@ def upload_argument_parser(parser_upload: argparse.ArgumentParser) -> argparse.A
         choices=["none", "aes"],
         default="none",
         help='The encryption-method to use.',
-        )
+    )
     parser_upload.add_argument(
         '--password',
         help='The password to use for encryption. This should not be used as it can leak your password',
-        required='--encryption-method' in sys.argv
-        )
+        required=('--encryption-method' in sys.argv or '-e' in sys.argv)
+    )
     parser_upload.add_argument(
         '--password-file',
         help='The path to the password-file for encryption',
-        required='--encryption-method' in sys.argv and '--encryption-password' not in sys.argv)
+        required=('--encryption-method' in sys.argv or '-e' in sys.argv) and '--password' not in sys.argv
+    )
     # Filetype
     parser_upload.add_argument(
         '-f',
@@ -45,7 +46,7 @@ def upload_argument_parser(parser_upload: argparse.ArgumentParser) -> argparse.A
         choices=["zip"],
         default="zip",
         help='The filetype to use.',
-        )
+    )
     # Transfer
     parser_upload.add_argument(
         '-t',
@@ -53,20 +54,20 @@ def upload_argument_parser(parser_upload: argparse.ArgumentParser) -> argparse.A
         choices=["save", "glacier"],
         help='The transfer-method to use.',
         required='--profile' not in sys.argv
-        )
+    )
     parser_upload.add_argument(
         '-s',
         '--transfer-chunk-size',
         default=64,
         type=int,
         help='The chunk-size to use for the transfer-method',
-        )
+    )
     # Dryrun
     parser_upload.add_argument(
         '--dryrun',
         action='store_true',
         help='If set, the files will not be uploaded'
-        )
+    )
     # Upload-Paths
     parser_upload.add_argument(
         '-p',
@@ -74,14 +75,15 @@ def upload_argument_parser(parser_upload: argparse.ArgumentParser) -> argparse.A
         nargs='+',
         help='Paths of the Files and Folders to upload',
         required=True
-        )
+    )
     return parser_upload
 
 def download_argument_parser(parser_download: argparse.ArgumentParser) -> argparse.ArgumentParser:
     parser_download.add_argument(
         '--id',
-        help='Id of Element to download'
-        )
+        help='Id of Element to download or Glacier Archive-Id',
+        required=True
+    )
     parser_download.add_argument(
         '--location', 
         default='.',
@@ -89,12 +91,10 @@ def download_argument_parser(parser_download: argparse.ArgumentParser) -> argpar
     )
     parser_download.add_argument(
         '--password', 
-        help='The password to use for encryption. This should not be used as it can leak your password',
-        required='--encryption-method' in sys.argv
+        help='The password to use for decryption. Only needed if the file is encrypted. Use with caution as it can leak your password',
     )
     parser_download.add_argument(
         '--password-file', 
-        help='The path to the password-file for encryption',
-        required='--encryption-method' in sys.argv and '--encryption-password' not in sys.argv
+        help='The path to the password-file for decryption. Only needed if the file is encrypted',
     )
     return parser_download
